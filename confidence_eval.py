8d1d13c9a58610c3ca17482a0a291b79a21cb908
dj <204916617@qq.com>
2023-07-12 18:51:06 +0800

    first

---
+++ confidence_eval.py
@@ -0,0 +1,205 @@
+import copy
+import os
+import torch
+from argparse import ArgumentParser, Namespace
+from rdkit.Chem import RemoveHs
+from functools import partial
+import numpy as np
+import pandas as pd
+from rdkit import RDLogger
+from torch_geometric.loader import DataLoader
+import math
+
+from datasets.process_mols import write_mol_with_coords
+from utils.diffusion_utils import t_to_sigma as t_to_sigma_compl, get_t_schedule
+from utils.inference_utils import InferenceDataset, set_nones
+# from utils.sampling import randomize_position, sampling
+from utils.utils import get_model
+from utils.visualise import PDBFile
+from tqdm import tqdm
+from utils.diffusion_utils import set_time
+
+def sampling(data_list, device,confidence_model=None, confidence_data_list=None,
+             confidence_model_args=None, batch_size=32, no_final_step_noise=False):
+    N = len(data_list)
+    with torch.no_grad():
+        if confidence_model is not None:
+            loader = DataLoader(data_list, batch_size=batch_size)
+            confidence_loader = iter(DataLoader(confidence_data_list, batch_size=batch_size))
+            confidence = []
+            for complex_graph_batch in loader:
+                complex_graph_batch = complex_graph_batch.to(device)
+                if confidence_data_list is not None:
+                    confidence_complex_graph_batch = next(confidence_loader).to(device)
+                    confidence_complex_graph_batch['ligand'].pos = complex_graph_batch['ligand'].pos
+                    set_time(confidence_complex_graph_batch, 0, 0, 0, N, confidence_model_args.all_atoms, device)
+                    confidence.append(confidence_model(confidence_complex_graph_batch))
+                else:
+                    confidence.append(confidence_model(complex_graph_batch))
+            confidence = torch.cat(confidence, dim=0)
+        else:
+            confidence = None
+
+    return data_list, confidence
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
+confidence_result={}
+os.chdir('/home/dj/diffdock/DiffDock-main')
+
+
+RDLogger.DisableLog('rdApp.*')
+import yaml
+parser = ArgumentParser()
+parser.add_argument('--protein_ligand_csv', type=str, default='/home/dj/diffdock/DiffDock-main/data/protein_ligand_example_csv.csv', help='Path to a .csv file specifying the input as described in the README. If this is not None, it will be used instead of the --protein_path, --protein_sequence and --ligand parameters')
+parser.add_argument('--complex_name', type=str, default='1a0q', help='Name that the complex will be saved with')
+parser.add_argument('--protein_path', type=str, default=None, help='Path to the protein file')
+parser.add_argument('--protein_sequence', type=str, default=None, help='Sequence of the protein for ESMFold, this is ignored if --protein_path is not None')
+parser.add_argument('--ligand_description', type=str, default='CCCCC(NC(=O)CCC(=O)O)P(=O)(O)OC1=CC=CC=C1', help='Either a SMILES string or the path to a molecule file that rdkit can read')
+
+parser.add_argument('--out_dir', type=str, default='results/user_inference', help='Directory where the outputs will be written to')
+# parser.add_argument('--save_visualisation', action='store_true', default=False, help='Save a pdb file with all of the steps of the reverse diffusion')
+# parser.add_argument('--samples_per_complex', type=int, default=10, help='Number of samples to generate')
+
+parser.add_argument('--model_dir', type=str, default='workdir/paper_score_model', help='Path to folder with trained score model and hyperparameters')
+parser.add_argument('--ckpt', type=str, default='best_ema_inference_epoch_model.pt', help='Checkpoint to use for the score model')
+parser.add_argument('--confidence_model_dir', type=str, default='workdir/paper_confidence_model', help='Path to folder with trained confidence model and hyperparameters')
+parser.add_argument('--confidence_ckpt', type=str, default='best_model_epoch75.pt', help='Checkpoint to use for the confidence model')
+
+parser.add_argument('--batch_size', type=int, default=32, help='')
+# parser.add_argument('--no_final_step_noise', action='store_true', default=False, help='Use no noise in the final step of the reverse diffusion')
+# parser.add_argument('--inference_steps', type=int, default=20, help='Number of denoising steps')
+# parser.add_argument('--actual_steps', type=int, default=None, help='Number of denoising steps that are actually performed')
+args = parser.parse_args()
+
+os.makedirs(args.out_dir, exist_ok=True)
+
+# ###score model
+# with open(f'{args.model_dir}/model_parameters.yml') as f:
+#     score_model_args = Namespace(**yaml.full_load(f))
+
+##### confidence_model
+if args.confidence_model_dir is not None:
+    with open(f'{args.confidence_model_dir}/model_parameters.yml') as f:
+        confidence_args = Namespace(**yaml.full_load(f))
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+####输入受体以及配体信息
+if args.protein_ligand_csv is not None:
+    df = pd.read_csv(args.protein_ligand_csv)
+    complex_name_list = set_nones(df['complex_name'].tolist())
+    protein_path_list = set_nones(df['protein_path'].tolist())
+    protein_sequence_list = set_nones(df['protein_sequence'].tolist())
+    ligand_description_list = set_nones(df['ligand_description'].tolist())
+else:
+    complex_name_list = [args.complex_name]
+    protein_path_list = [args.protein_path]
+    protein_sequence_list = [args.protein_sequence]
+    ligand_description_list = [args.ligand_description]
+
+complex_name_list = [name if name is not None else f"complex_{i}" for i, name in enumerate(complex_name_list)]
+for name in complex_name_list:
+    write_dir = f'{args.out_dir}/{name}'
+    os.makedirs(write_dir, exist_ok=True)
+
+###转换受体以及配体为图
+# test_dataset = InferenceDataset(out_dir=args.out_dir, complex_names=complex_name_list, protein_files=protein_path_list,
+#                                 ligand_descriptions=ligand_description_list, protein_sequences=protein_sequence_list,
+#                                 lm_embeddings=score_model_args.esm_embeddings_path is not None,
+#                                 receptor_radius=score_model_args.receptor_radius, remove_hs=score_model_args.remove_hs,
+#                                 c_alpha_max_neighbors=score_model_args.c_alpha_max_neighbors,
+#                                 all_atoms=score_model_args.all_atoms, atom_radius=score_model_args.atom_radius,
+#                                 atom_max_neighbors=score_model_args.atom_max_neighbors)
+
+if args.confidence_model_dir is not None and not confidence_args.use_original_model_cache:
+    print('HAPPENING | confidence model uses different type of graphs than the score model. '
+          'Loading (or creating if not existing) the data for the confidence model now.')
+    confidence_test_dataset = \
+        InferenceDataset(out_dir=args.out_dir, complex_names=complex_name_list, protein_files=protein_path_list,
+                         ligand_descriptions=ligand_description_list, protein_sequences=protein_sequence_list,
+                         lm_embeddings=confidence_args.esm_embeddings_path is not None,
+                         receptor_radius=confidence_args.receptor_radius, remove_hs=confidence_args.remove_hs,
+                         c_alpha_max_neighbors=confidence_args.c_alpha_max_neighbors,
+                         all_atoms=confidence_args.all_atoms, atom_radius=confidence_args.atom_radius,
+                         atom_max_neighbors=confidence_args.atom_max_neighbors,
+                         precomputed_lm_embeddings=None)
+else:
+    confidence_test_dataset = None
+test_loader = DataLoader(dataset=confidence_test_dataset, batch_size=1, shuffle=False)
+
+##########？？？？？？？？？？？？？？？？
+# t_to_sigma = partial(t_to_sigma_compl, args=score_model_args)
+
+#model = get_model(score_model_args, device, t_to_sigma=t_to_sigma, no_parallel=True)
+# state_dict = torch.load(f'{args.model_dir}/{args.ckpt}', map_location=torch.device('cpu'))
+# model.load_state_dict(state_dict, strict=True)
+# model = model.to(device)
+# model.eval()
+
+if args.confidence_model_dir is not None:
+    confidence_model = get_model(confidence_args, device, t_to_sigma=[0,0,0], no_parallel=True, confidence_mode=True)
+    state_dict = torch.load(f'{args.confidence_model_dir}/{args.confidence_ckpt}', map_location=torch.device('cpu'))
+    confidence_model.load_state_dict(state_dict, strict=True)
+    confidence_model = confidence_model.to(device)
+    confidence_model.eval()
+else:
+    confidence_model = None
+    confidence_args = None
+
+# tr_schedule = get_t_schedule(inference_steps=args.inference_steps)
+
+failures, skipped = 0, 0
+# N = args.samples_per_complex
+# print('Size of test dataset: ', len(test_dataset))
+for idx, orig_complex_graph in tqdm(enumerate(test_loader)):
+    try:
+        if confidence_test_dataset is not None:
+            confidence_complex_graph = confidence_test_dataset[idx]
+            if not confidence_complex_graph.success:
+                skipped += 1
+                print(f"HAPPENING | The confidence dataset did not contain {orig_complex_graph.name}. We are skipping this complex.")
+                continue
+            confidence_data_list = [confidence_complex_graph]
+            # confidence_data_list = [copy.deepcopy(confidence_complex_graph) for _ in range(N)]
+        else:
+            confidence_data_list = None
+        data_list = [orig_complex_graph]
+        # data_list = [copy.deepcopy(orig_complex_graph) for _ in range(N)]
+        # randomize_position(data_list, score_model_args.no_torsion, False, score_model_args.tr_sigma_max)
+        lig = orig_complex_graph.mol[0]
+
+        # run reverse diffusion
+        data_list, confidence = sampling(data_list=data_list,
+                                         device=device,confidence_model=confidence_model,
+                                         confidence_data_list=confidence_data_list, confidence_model_args=confidence_args,
+                                         batch_size=args.batch_size)
+        ligand_pos = np.asarray([complex_graph['ligand'].pos.cpu().numpy() + orig_complex_graph.original_center.cpu().numpy() for complex_graph in data_list])
+        
+        # reorder predictions based on confidence output
+        if confidence is not None and isinstance(confidence_args.rmsd_classification_cutoff, list):
+            confidence = confidence[:, 0]
+        if confidence is not None:
+            confidence = confidence.cpu().numpy()
+            print(confidence)
+            confidence_result[confidence_test_dataset.ligand_descriptions[idx]]=sigmoid(confidence)
+            re_order = np.argsort(confidence)[::-1]
+            confidence = confidence[re_order]
+            ligand_pos = ligand_pos[re_order]
+        # # save visualisation frames
+        # if args.save_visualisation:
+        #     if confidence is not None:
+        #         for rank, batch_idx in enumerate(re_order):
+        #             visualization_list[batch_idx].write(os.path.join(write_dir, f'rank{rank+1}_reverseprocess.pdb'))
+        #     else:
+        #         for rank, batch_idx in enumerate(ligand_pos):
+        #             visualization_list[batch_idx].write(os.path.join(write_dir, f'rank{rank+1}_reverseprocess.pdb'))
+
+    except Exception as e:
+        print("Failed on",complex_name_list[idx] , e)
+        failures += 1
+
+print(f'Failed for {failures} complexes')
+print(f'Skipped {skipped} complexes')
+print(f'Results are in {args.out_dir}')
+
+
---
+++ randomize_pos.py
@@ -0,0 +1,260 @@
+import os
+from rdkit import Chem
+from rdkit.Chem.rdchem import BondType as BT
+from Bio.PDB import PDBParser
+from esm import FastaBatchedDataset, pretrained
+from rdkit.Chem import AddHs, MolFromSmiles
+from torch_geometric.data import Dataset, HeteroData
+import warnings
+import copy
+import warnings
+import numpy as np
+import scipy.spatial as spa
+import torch
+from rdkit import Chem
+from rdkit.Chem.rdchem import BondType as BT
+from scipy import spatial
+from scipy.special import softmax
+from torch_cluster import radius_graph
+from scipy.spatial.transform import Rotation as R
+from rdkit.Chem import AllChem
+import torch.nn.functional as F
+
+from datasets.conformer_matching import get_torsion_angles, optimize_rotatable_bonds
+from utils.torsion import get_transformation_mask
+
+def safe_index(l, e):
+    """ Return index of element e in list l. If e is not present, return the last index """
+    try:
+        return l.index(e)
+    except:
+        return len(l) - 1
+
+allowable_features = {
+    'possible_atomic_num_list': list(range(1, 119)) + ['misc'],
+    'possible_chirality_list': [
+        'CHI_UNSPECIFIED',
+        'CHI_TETRAHEDRAL_CW',
+        'CHI_TETRAHEDRAL_CCW',
+        'CHI_OTHER'
+    ],
+    'possible_degree_list': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'misc'],
+    'possible_numring_list': [0, 1, 2, 3, 4, 5, 6, 'misc'],
+    'possible_implicit_valence_list': [0, 1, 2, 3, 4, 5, 6, 'misc'],
+    'possible_formal_charge_list': [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 'misc'],
+    'possible_numH_list': [0, 1, 2, 3, 4, 5, 6, 7, 8, 'misc'],
+    'possible_number_radical_e_list': [0, 1, 2, 3, 4, 'misc'],
+    'possible_hybridization_list': [
+        'SP', 'SP2', 'SP3', 'SP3D', 'SP3D2', 'misc'
+    ],
+    'possible_is_aromatic_list': [False, True],
+    'possible_is_in_ring3_list': [False, True],
+    'possible_is_in_ring4_list': [False, True],
+    'possible_is_in_ring5_list': [False, True],
+    'possible_is_in_ring6_list': [False, True],
+    'possible_is_in_ring7_list': [False, True],
+    'possible_is_in_ring8_list': [False, True],
+    'possible_amino_acids': ['ALA', 'ARG', 'ASN', 'ASP', 'CYS', 'GLN', 'GLU', 'GLY', 'HIS', 'ILE', 'LEU', 'LYS', 'MET',
+                             'PHE', 'PRO', 'SER', 'THR', 'TRP', 'TYR', 'VAL', 'HIP', 'HIE', 'TPO', 'HID', 'LEV', 'MEU',
+                             'PTR', 'GLV', 'CYT', 'SEP', 'HIZ', 'CYM', 'GLM', 'ASQ', 'TYS', 'CYX', 'GLZ', 'misc'],
+    'possible_atom_type_2': ['C*', 'CA', 'CB', 'CD', 'CE', 'CG', 'CH', 'CZ', 'N*', 'ND', 'NE', 'NH', 'NZ', 'O*', 'OD',
+                             'OE', 'OG', 'OH', 'OX', 'S*', 'SD', 'SG', 'misc'],
+    'possible_atom_type_3': ['C', 'CA', 'CB', 'CD', 'CD1', 'CD2', 'CE', 'CE1', 'CE2', 'CE3', 'CG', 'CG1', 'CG2', 'CH2',
+                             'CZ', 'CZ2', 'CZ3', 'N', 'ND1', 'ND2', 'NE', 'NE1', 'NE2', 'NH1', 'NH2', 'NZ', 'O', 'OD1',
+                             'OD2', 'OE1', 'OE2', 'OG', 'OG1', 'OH', 'OXT', 'SD', 'SG', 'misc'],
+}
+bonds = {BT.SINGLE: 0, BT.DOUBLE: 1, BT.TRIPLE: 2, BT.AROMATIC: 3}
+
+lig_feature_dims = (list(map(len, [
+    allowable_features['possible_atomic_num_list'],
+    allowable_features['possible_chirality_list'],
+    allowable_features['possible_degree_list'],
+    allowable_features['possible_formal_charge_list'],
+    allowable_features['possible_implicit_valence_list'],
+    allowable_features['possible_numH_list'],
+    allowable_features['possible_number_radical_e_list'],
+    allowable_features['possible_hybridization_list'],
+    allowable_features['possible_is_aromatic_list'],
+    allowable_features['possible_numring_list'],
+    allowable_features['possible_is_in_ring3_list'],
+    allowable_features['possible_is_in_ring4_list'],
+    allowable_features['possible_is_in_ring5_list'],
+    allowable_features['possible_is_in_ring6_list'],
+    allowable_features['possible_is_in_ring7_list'],
+    allowable_features['possible_is_in_ring8_list'],
+])), 0)  # number of scalar features
+
+rec_atom_feature_dims = (list(map(len, [
+    allowable_features['possible_amino_acids'],
+    allowable_features['possible_atomic_num_list'],
+    allowable_features['possible_atom_type_2'],
+    allowable_features['possible_atom_type_3'],
+])), 0)
+
+rec_residue_feature_dims = (list(map(len, [
+    allowable_features['possible_amino_acids']
+])), 0)
+
+def lig_atom_featurizer(mol):
+    ringinfo = mol.GetRingInfo()
+    atom_features_list = []
+    for idx, atom in enumerate(mol.GetAtoms()):
+        atom_features_list.append([
+            safe_index(allowable_features['possible_atomic_num_list'], atom.GetAtomicNum()),##获取原子对应的元素编号
+            allowable_features['possible_chirality_list'].index(str(atom.GetChiralTag())),##原子的手性信息
+            safe_index(allowable_features['possible_degree_list'], atom.GetTotalDegree()),###原子参与形成键的数目
+            safe_index(allowable_features['possible_formal_charge_list'], atom.GetFormalCharge()),##原子的电荷
+            safe_index(allowable_features['possible_implicit_valence_list'], atom.GetImplicitValence()),##获取原子隐式化合价
+            safe_index(allowable_features['possible_numH_list'], atom.GetTotalNumHs()),
+            safe_index(allowable_features['possible_number_radical_e_list'], atom.GetNumRadicalElectrons()),
+            safe_index(allowable_features['possible_hybridization_list'], str(atom.GetHybridization())),
+            allowable_features['possible_is_aromatic_list'].index(atom.GetIsAromatic()),
+            safe_index(allowable_features['possible_numring_list'], ringinfo.NumAtomRings(idx)),
+            allowable_features['possible_is_in_ring3_list'].index(ringinfo.IsAtomInRingOfSize(idx, 3)),
+            allowable_features['possible_is_in_ring4_list'].index(ringinfo.IsAtomInRingOfSize(idx, 4)),
+            allowable_features['possible_is_in_ring5_list'].index(ringinfo.IsAtomInRingOfSize(idx, 5)),
+            allowable_features['possible_is_in_ring6_list'].index(ringinfo.IsAtomInRingOfSize(idx, 6)),
+            allowable_features['possible_is_in_ring7_list'].index(ringinfo.IsAtomInRingOfSize(idx, 7)),
+            allowable_features['possible_is_in_ring8_list'].index(ringinfo.IsAtomInRingOfSize(idx, 8)),
+        ])
+
+    return torch.tensor(atom_features_list)
+def read_molecule(molecule_file, sanitize=False, calc_charges=False, remove_hs=False):
+    if molecule_file.endswith('.mol2'):
+        mol = Chem.MolFromMol2File(molecule_file, sanitize=False, removeHs=False)
+    elif molecule_file.endswith('.sdf'):
+        supplier = Chem.SDMolSupplier(molecule_file, sanitize=False, removeHs=False)
+        mol = supplier[0]
+    elif molecule_file.endswith('.pdbqt'):
+        with open(molecule_file) as file:
+            pdbqt_data = file.readlines()
+        pdb_block = ''
+        for line in pdbqt_data:
+            pdb_block += '{}\n'.format(line[:66])
+        mol = Chem.MolFromPDBBlock(pdb_block, sanitize=False, removeHs=False)
+    elif molecule_file.endswith('.pdb'):
+        mol = Chem.MolFromPDBFile(molecule_file, sanitize=False, removeHs=False)
+    else:
+        raise ValueError('Expect the format of the molecule_file to be '
+                         'one of .mol2, .sdf, .pdbqt and .pdb, got {}'.format(molecule_file))
+
+    try:
+        if sanitize or calc_charges:
+            Chem.SanitizeMol(mol)
+
+        if calc_charges:
+            # Compute Gasteiger charges on the molecule.
+            try:
+                AllChem.ComputeGasteigerCharges(mol)
+            except:
+                warnings.warn('Unable to compute charges for the molecule.')
+    except Exception as e:
+        print(e)
+        print("RDKit was unable to read the molecule.")
+        return None
+    
+def get_lig_graph(mol, complex_graph):
+    lig_coords = torch.from_numpy(mol.GetConformer().GetPositions()).float()
+    atom_feats = lig_atom_featurizer(mol)
+
+    row, col, edge_type = [], [], []
+    for bond in mol.GetBonds():
+        start, end = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
+        row += [start, end]
+        col += [end, start]
+        edge_type += 2 * [bonds[bond.GetBondType()]] if bond.GetBondType() != BT.UNSPECIFIED else [0, 0]
+
+    edge_index = torch.tensor([row, col], dtype=torch.long)
+    edge_type = torch.tensor(edge_type, dtype=torch.long)
+    edge_attr = F.one_hot(edge_type, num_classes=len(bonds)).to(torch.float)
+
+    complex_graph['ligand'].x = atom_feats
+    complex_graph['ligand'].pos = lig_coords
+    complex_graph['ligand', 'lig_bond', 'ligand'].edge_index = edge_index
+    complex_graph['ligand', 'lig_bond', 'ligand'].edge_attr = edge_attr
+    return
+def get_lig_graph_with_matching(mol_, complex_graph):
+    complex_graph.rmsd_matching = 0
+    get_lig_graph(mol_, complex_graph)
+
+    edge_mask, mask_rotate = get_transformation_mask(complex_graph)
+    complex_graph['ligand'].edge_mask = torch.tensor(edge_mask)
+    complex_graph['ligand'].mask_rotate = mask_rotate
+
+    return
+def modify_conformer_torsion_angles(pos, edge_index, mask_rotate, torsion_updates, as_numpy=False):
+    pos = copy.deepcopy(pos)
+    if type(pos) != np.ndarray: pos = pos.cpu().numpy()
+
+    for idx_edge, e in enumerate(edge_index.cpu().numpy()):
+        if torsion_updates[idx_edge] == 0:
+            continue
+        u, v = e[0], e[1]
+
+        # check if need to reverse the edge, v should be connected to the part that gets rotated
+        assert not mask_rotate[idx_edge, u]
+        assert mask_rotate[idx_edge, v]
+
+        rot_vec = pos[u] - pos[v]  # convention: positive rotation if pointing inwards
+        rot_vec = rot_vec * torsion_updates[idx_edge] / np.linalg.norm(rot_vec) # idx_edge!
+        rot_mat = R.from_rotvec(rot_vec).as_matrix()
+
+        pos[mask_rotate[idx_edge]] = (pos[mask_rotate[idx_edge]] - pos[v]) @ rot_mat.T + pos[v]
+
+    if not as_numpy: pos = torch.from_numpy(pos.astype(np.float32))
+    return pos
+
+def randomize_position(data_list, no_torsion, no_random, tr_sigma_max):
+    # in place modification of the list
+    if not no_torsion:
+        # randomize torsion angles
+        for complex_graph in data_list:
+            torsion_updates = np.random.uniform(low=-np.pi, high=np.pi, size=complex_graph['ligand'].edge_mask.sum())
+            complex_graph['ligand'].pos = \
+                modify_conformer_torsion_angles(complex_graph['ligand'].pos,
+                                                complex_graph['ligand', 'ligand'].edge_index.T[
+                                                    complex_graph['ligand'].edge_mask],
+                                                complex_graph['ligand'].mask_rotate[0], torsion_updates)
+
+    for complex_graph in data_list:
+        # randomize position
+        molecule_center = torch.mean(complex_graph['ligand'].pos, dim=0, keepdim=True)
+        random_rotation = torch.from_numpy(R.random().as_matrix()).float()
+        complex_graph['ligand'].pos = (complex_graph['ligand'].pos - molecule_center) @ random_rotation.T
+        # base_rmsd = np.sqrt(np.sum((complex_graph['ligand'].pos.cpu().numpy() - orig_complex_graph['ligand'].pos.numpy()) ** 2, axis=1).mean())
+
+        if not no_random:  # note for now the torsion angles are still randomised
+            tr_update = torch.normal(mean=0, std=tr_sigma_max, size=(1, 3))
+            complex_graph['ligand'].pos += tr_update
+
+
+name=''
+ligand_description=''
+
+# build the pytorch geometric heterogeneous graph
+complex_graph = HeteroData()
+complex_graph['name'] = name
+# parse the ligand, either from file or smile
+try:
+    mol = MolFromSmiles(ligand_description)  # check if it is a smiles or a path
+    if mol is not None:
+        mol = AddHs(mol)
+        # generate_conformer(mol)
+    else:
+        mol = read_molecule(ligand_description, remove_hs=False, sanitize=True)
+    if mol is None:
+        raise Exception('RDKit could not read the molecule ', ligand_description)
+                # mol.RemoveAllConformers()
+                # mol = AddHs(mol)
+                # generate_conformer(mol)
+except Exception as e:
+    print('Failed to read molecule ', ligand_description, ' We are skipping it. The reason is the exception: ', e)
+    complex_graph['success'] = False
+
+get_lig_graph_with_matching(mol, complex_graph, popsize=None, maxiter=None, matching=False, keep_original=False,
+                                        num_conformers=1)
+
+ligand_center = torch.mean(complex_graph['ligand'].pos, dim=0, keepdim=True)
+complex_graph['ligand'].pos -= ligand_center
+complex_graph.mol = mol
+randomize_position(complex_graph, score_model_args.no_torsion, False, score_model_args.tr_sigma_max)
